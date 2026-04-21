0bd983327ac6bb3c87b958798d3812b055d9d126
{sharath} <sharathkumarchelimella@gmail.com>
2026-04-21 19:30:51 +0530

    Removed unnecessary files

--- arrays/arrays_basics.java/q5_Majority.java
+++
@@ -1,9 +0,0 @@
-//169. Majority Element
-
-class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int key = nums.length/2;
-        return nums[key];
-    }
-}
