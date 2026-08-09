138fcc47154dfdaffb6051468daff3f09bc223c7
Sharath Kumar <Sharathkumarchellimela@gmail.com>
2026-08-09 22:38:16 +0530

    Add continuous subarray sum solution

---
+++ DSA-Interview-/arrays/Arrays/03_Prefix_sum/03_Continuous_sum_subarray.java
@@ -0,0 +1,22 @@
+//523. Continuous Subarray Sum
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        map.put(0,-1);
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            int rem = sum%k;
+            if(map.containsKey(rem)){
+                if(2 <= i-map.get(rem)){
+                    return true;
+                }
+            }else{
+                map.put(rem,i);
+            };
+        }
+        return false;
+        
+    }
+}
--- Recursion/Basics/Main.java
+++ Recursion/Basics/Main.java
@@ -299,3 +299,26 @@ public class Main{
 	    
 	}
 }
+
+
+//50. Pow(x, n)
+class Solution {
+    public double myPow(double x, int n) {
+        long pow = n;
+        if(pow < 0){
+            x = 1/x;
+            pow = -pow;
+        }
+        return power(x,pow,1);
+    }
+    public static double power(double x,long n,double ans){  
+        if(n==0){
+            return ans;
+        }
+        if(n%2 != 0 ){
+            ans *= x;
+        }
+        return power(x*x,n/2,ans);
+    }
+}
+
